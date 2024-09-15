ecf9fa29c57e71f98f8f71df13454c18315c0dd0
Maria Leitão <malufln.99@gmail.com>
2024-09-15 13:39:54 -0300

    feat: add user repository

---
+++ src/main/java/com/maria/pombo/model/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.maria.pombo.model.repository;
+
+import com.maria.pombo.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
