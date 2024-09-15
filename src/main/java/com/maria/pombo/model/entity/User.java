de2113524cfd63d5f7e8152e98a950451ebe06f3
Maria Leitão <malufln.99@gmail.com>
2024-09-15 11:26:40 -0300

    feat: add user entity

---
+++ src/main/java/com/maria/pombo/model/entity/User.java
@@ -0,0 +1,45 @@
+package com.maria.pombo.model.entity;
+
+import com.maria.pombo.model.entity.enums.UserRoles;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity(name = "users")
+@Data
+public class User implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+            flags = Pattern.Flag.CASE_INSENSITIVE, message = "Email should be valid")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "CPF is required")
+    @CPF(message = "CPF should be valid")
+    @Column(unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRoles role = UserRoles.USER;
+
+}
