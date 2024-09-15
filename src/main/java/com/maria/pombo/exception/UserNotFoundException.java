22de2dd9f2f807558a657cd5abcaf1db046d07db
Maria Leitão <malufln.99@gmail.com>
2024-09-15 13:54:37 -0300

    feat: add UserNotFoundException for user-related errors

---
+++ src/main/java/com/maria/pombo/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.maria.pombo.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
